0cbb7d812c4ca7eb0abe48dfcf7c0dbad66829ab
Piyush-Code13 <work.piyushbande@gmail.com>
2026-07-31 00:46:50 +0530

    Update config.py to support local, Docker, and Streamlit Cloud credentials

--- config.py
+++ config.py
@@ -1,16 +1,32 @@
-from google.cloud import bigquery
-from google.oauth2 import service_account
+import os
 import streamlit as st
 
-# Project ID
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
 PROJECT_ID = "bizanalytics-project"
 
-# Read credentials from Streamlit Secrets
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
+# -------------------------------
+# Streamlit Cloud
+# -------------------------------
+if "gcp_service_account" in st.secrets:
 
-# BigQuery client
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+
+# -------------------------------
+# Local / Docker
+# -------------------------------
+else:
+
+    credentials = service_account.Credentials.from_service_account_file(
+        os.getenv(
+            "GOOGLE_APPLICATION_CREDENTIALS",
+            "credentials/gcp-key.json"
+        )
+    )
+
 client = bigquery.Client(
     credentials=credentials,
     project=PROJECT_ID
