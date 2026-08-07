eeffba307d79c5ce2480eced9dd231da33b3f19c
Piyush-Code13 <work.piyushbande@gmail.com>
2026-08-07 19:16:50 +0530

    Add Docker support, project screenshots

--- config.py
+++ config.py
@@ -4,22 +4,16 @@ import streamlit as st
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
-PROJECT_ID = "bizanalytics-project"
+PROJECT_ID = os.getenv("GCP_PROJECT_ID", "bizanalytics-project")
 
-# -------------------------------
-# Streamlit Cloud
-# -------------------------------
-if "gcp_service_account" in st.secrets:
-
+try:
+    # Streamlit Cloud
     credentials = service_account.Credentials.from_service_account_info(
         st.secrets["gcp_service_account"]
     )
 
-# -------------------------------
-# Local / Docker
-# -------------------------------
-else:
-
+except Exception:
+    # Local / Docker
     credentials = service_account.Credentials.from_service_account_file(
         os.getenv(
             "GOOGLE_APPLICATION_CREDENTIALS",
